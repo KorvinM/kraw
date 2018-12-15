b302e5f79af2f43f9753ae09cc0ea2bb6c46110d
KorvinM <km@korvin.org>
2018-12-15 12:26:55 +0000

    add mirror and mandala modes

--- main.js
+++ main.js
@@ -16,10 +16,15 @@ function init(){
 }
 
 function getColor() {
-    return document.querySelector(".color").value;
+  return document.querySelector(".color").value;
 }
+
 function getLineWidth() {
-    return document.querySelector("#line-width").value;
+  return document.querySelector("#line-width").value;
+}
+
+function getMode() {
+  return document.querySelector("#mode").value;
 }
 
 function clearCanvas() {
@@ -33,6 +38,12 @@ function drawLine() {
         b = prevY,
         c = currX,
         d = currY;
+    if (getMode()=="mirror"||getMode()=="mandala"){
+	var a_ = a, 
+	    b_ = h-b,
+            c_ = c,
+	    d_ = h-d;
+	}
 
     context.strokeStyle = getColor();
     context.lineWidth = getLineWidth();
@@ -41,6 +52,59 @@ function drawLine() {
     context.beginPath();
     context.moveTo(a, b);
     context.lineTo(c, d);
+
+    if (getMode()=="mirror"||getMode()=="mandala"){
+	context.moveTo(a_, b_);
+        context.lineTo(c_, d_);
+	}
+    if (getMode()=="mandala"){
+	// Reassign values
+    a_ = w-a; b_ = b;
+    c_ = w-c; d_ = d;
+
+    // Draw the 3rd line
+    context.moveTo(a_, b_);
+    context.lineTo(c_, d_);
+
+    // Reassign values
+    a_ = w-a; b_ = h-b;
+    c_ = w-c; d_ = h-d;
+
+    // Draw the 4th line
+    context.moveTo(a_, b_);
+    context.lineTo(c_, d_);
+
+    // Reassign for 5
+    a_ = w/2+h/2-b; b_ = w/2+h/2-a;
+    c_ = w/2+h/2-d; d_ = w/2+h/2-c;
+
+    //draw 5
+    context.moveTo(a_, b_);
+    context.lineTo(c_, d_);
+
+    // Reassign for 6
+    a_ = w/2+h/2-b; b_ = h/2-w/2+a;
+    c_ = w/2+h/2-d; d_ = h/2-w/2+c;
+
+    //draw 6 
+    context.moveTo(a_, b_);
+    context.lineTo(c_, d_);
+
+    // Reassign for 7
+    a_ = w/2-h/2+b; b_ = w/2+h/2-a;
+    c_ = w/2-h/2+d; d_ = w/2+h/2-c;
+    //draw 7 
+    context.moveTo(a_, b_);
+    context.lineTo(c_, d_);
+
+    // Reassign for 8
+    a_ = w/2-h/2+b; b_ = h/2-w/2+a;
+    c_ = w/2-h/2+d; d_ = h/2-w/2+c;
+    //draw 8 
+    context.moveTo(a_, b_);
+    context.lineTo(c_, d_);
+
+}
     context.stroke();
     context.closePath();
 }
